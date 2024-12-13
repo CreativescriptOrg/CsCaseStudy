6059d3e458e9feb4aa4bc75b56871ac171f3b345
Sarthak <sarthak2019fractal@gmail.com>
2024-12-13 14:16:56 +0530

    First Commit

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,15 @@
 import styles from "./styles.module.css";
 
 const Hero = () => {
-	return (
-		<div className={styles.container}>
-			<h1>Case Study Title</h1>
-			<img src='/images/hero.png' alt='' />
-			<div className={styles.backdrop}></div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <h1>Case Study Title</h1>
+      <img src="/images/hero.png" alt="" />
+      <div className={styles.backdrop}></div>
+    </div>
+  );
 };
 
 export default Hero;
+
+// throw
