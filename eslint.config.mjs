6b9326f2382ce15a3ad3ccc1b4d9743a7c4141e1
Sarthak <sarthak2019fractal@gmail.com>
2025-01-08 23:17:13 +0530

    Fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,17 +6,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname, // Ensure the base directory is correct
 });
 
-const eslintConfig = [
+export default [
   ...compat.config({
-    extends: ["next"],
+    extends: ["plugin:@next/next/recommended"],
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
     },
   }),
 ];
-
-export default eslintConfig;
