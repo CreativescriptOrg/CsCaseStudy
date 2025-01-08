cff129556fcc9f23eff952446765658d9a3ef647
karishma-dev <jainkarishma76@gmail.com>
2025-01-08 22:32:50 +0530

    updated homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,41 +2,37 @@ import data from "../case_study_1.json";
 
 export default function Home() {
 	return (
-		<div>
-			Go to
-			<br />
-			<a href='/casestudy/konster'>Konster</a>
-			<br />
-			<a href='/casestudy/docchase'>docchase</a>
-			<br />
-			<a href='/casestudy/cumulus'>Cumulus</a>
-			<br />
-			<a href='/casestudy/polyviam'>Polyviam</a>
-			<br />
-			<a href='/casestudy/second-sathi'>Second Sathi</a>
-			<br />
-			<a href='/casestudy/taie'>Taie</a>
-			<br />
-			<a href='/casestudy/fuzzle'>Fuzzle</a>
-			<br />
-			<a href='/casestudy/dolado'>Dolado</a>
-			<br />
-			<a href='/casestudy/real-estate'>Real Estate</a>
-			<br />
-			<a href='/casestudy/cubu'>Cubu</a>
-			<br />
-			<a href='/casestudy/one-nation-innovation'>One nation</a>
-			<br />
-			<a href='/casestudy/poker-zombie'>Poker zombie</a>
-			<br />
-			<a href='/casestudy/seoul'>Seoul Medical CRM</a>
-			<br />
-			<a href='/casestudy/eventflow'>Event Flow</a>
-			<br />
-			<a href='/casestudy/estu'>Estu</a>
-			<br />
-			<a href='/casestudy/game-menu'>Game Menu</a>
-			<br />
+		<div className='container website_list'>
+			<h1>Case Studies</h1>
+			<ul>
+				{[
+					{ href: "/casestudy/konster", label: "Konster" },
+					{ href: "/casestudy/docchase", label: "Docchase" },
+					{ href: "/casestudy/cumulus", label: "Cumulus" },
+					{ href: "/casestudy/polyviam", label: "Polyviam" },
+					{ href: "/casestudy/second-sathi", label: "Second Sathi" },
+					{ href: "/casestudy/taie", label: "Taie" },
+					{ href: "/casestudy/fuzzle", label: "Fuzzle" },
+					{ href: "/casestudy/dolado", label: "Dolado" },
+					{ href: "/casestudy/real-estate", label: "Real Estate" },
+					{ href: "/casestudy/cubu", label: "Cubu" },
+					{ href: "/casestudy/one-nation-innovation", label: "One Nation" },
+					{ href: "/casestudy/poker-zombie", label: "Poker Zombie" },
+					{ href: "/casestudy/seoul", label: "Seoul Medical CRM" },
+					{ href: "/casestudy/eventflow", label: "Event Flow" },
+					{ href: "/casestudy/estu", label: "Estu" },
+					{ href: "/casestudy/game-menu", label: "Game Menu" },
+				].map((item, index) => (
+					<li key={index} style={{ margin: "10px 0" }}>
+						<a
+							href={item.href}
+							style={{ textDecoration: "none", color: "#0070f3" }}
+						>
+							{item.label}
+						</a>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
