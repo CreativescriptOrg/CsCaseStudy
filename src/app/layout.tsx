5700b1ad32a780c3118b8a0b3a3e69bf6a839315
karishma-dev <jainkarishma76@gmail.com>
2025-02-13 21:31:31 +0530

    updated images, and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { EB_Garamond } from "next/font/google";
+
+const ebGaramond = EB_Garamond({
+	subsets: ["latin"],
+	variable: "--font-eb-garamond",
+	weight: ["400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
-  title: "Case Study",
-  description: "",
+	title: "Case Study",
+	description: "",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='en' className={ebGaramond.variable}>
+			<body>{children}</body>
+		</html>
+	);
 }
